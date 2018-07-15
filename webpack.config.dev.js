cdc2a668db87fb6944b4056105a845f5f805eec7
노경모 <kyeongmo2@gmail.com>
2018-07-16 00:08:52 +0900

    Add webpack config setting file for development and install dependencies

---
+++ webpack.config.dev.js
@@ -0,0 +1,182 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://0.0.0.0:8000',
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'src/index.js')
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    pathinfo: true,
+    filename: 'js/bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // Apply Eslint
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint')
+            },
+            loader: require.resolve('eslint-loader')
+          }
+        ],
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'media/[name].[hash:8].[ext]'
+            }
+          },
+          {
+            test: /\.(js|jsx)$/,
+            include: path.resolve(__dirname, 'src'),
+            exclude: [/node_modules/],
+            loader: require.resolve('babel-loader'),
+            options: {
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.(css|scss)$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                  camelCase: 'dashes'
+                }
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require.resolve('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9'
+                      ],
+                      flexbox: 'no-2009'
+                    })
+                  ],
+                  sourceMap: true
+                }
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          },
+          {
+            exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'media/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      title: 'Visit Euroup',
+      inject: 'body',
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CaseSensitivePathsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new WatchMissingNodeModulesPlugin(path.resolve(__dirname, 'node_modules')),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ],
+
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  },
+
+  devServer: {
+    open: true,
+    host: '0.0.0.0',
+    port: 8000,
+    proxy: {
+      '/api/*': 'http://localhost:3000'
+    },
+    compress: true,
+    historyApiFallback: true,
+    inline: true,
+    overlay: true,
+    hot: true,
+    filename: 'js/bundle.js',
+    stats: {
+      assets: true,
+      children: true,
+      chunks: false,
+      chunksModule: false,
+      colors: true,
+      performance: false,
+      publicPath: true,
+      version: true,
+      hash: true,
+      timings: true,
+      warnings: false
+    }
+  },
+
+  performance: {
+    hints: false
+  }
+};
