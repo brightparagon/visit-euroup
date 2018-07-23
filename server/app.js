a7d9f44db064b2771b38f804edafa4c684a7d5d2
노경모 <kyeongmo2@gmail.com>
2018-07-24 00:17:29 +0900

    Add node server app and modify eslint config

---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+const dbUrl = process.env.MONGODB_URI;
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to mongodb server');
+});
+db.on('error', console.error);
+mongoose.connect(dbUrl, {
+  safe: true
+});
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendfile(path.resolve(__dirname, 'public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on port: ${port}`);
+});
